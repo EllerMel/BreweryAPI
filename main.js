5e0a57817b63b6b92d929bb9fa8e409974918718
Mae <msmaedev@gmail.com>
2018-11-19 20:28:54 -0500

    11192018 updates

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 document.getElementById('getData').addEventListener('click', getTheData)
 
-apiURL = 'https://api.openbrewerydb.org/breweries?by_city=Lexington&by_state=KY&per_page=15'
-
 function getTheData() {
+
+    city = document.getElementById('City').value;
+    console.log(city.value);
+    state = document.getElementById('State').value;
+    console.log(state.vale);
+
+    if (city != "" && state != ""){
+        apiURL = `https://api.openbrewerydb.org/breweries?by_city=${city}&by_state=${state}&per_page=15`
+    }
+    else if (city = "" && state != ""){
+        apiURL = `https://api.openbrewerydb.org/breweries?by_state=${state}&per_page=15`    
+    }
+    else if (city != "" && state == ""){
+        apiURL = `https://api.openbrewerydb.org/breweries?by_city=${city}&per_page=15`
+    }
+    apiURL = `https://api.openbrewerydb.org/breweries?by_city=Detroit&by_state=MI&per_page=15`
+
+    console.log(apiURL)
+
     fetch(apiURL)
         .then(function (response) {
             //console.log(response.json());
@@ -15,11 +32,11 @@ function getTheData() {
             data.map(function (i) {
                 brews += `
                         <ul>
-                        <li>${i.name}</li>
-                        <li>Brewery Type - ${i.brewery_type}</li>
-                        <li>${i.street} ${i.city}, ${i.state} ${i.postal_code}</li>
+                        <li><b>${i.name}</b></li>
+                        <li><Brewery Type - ${i.brewery_type}</li>
+                        <li>${i.street} ${i.city}, ${i.state} ${i.postal_code}</li>                   
                         <li>${i.phone}</li>   
-                        <li>${i.website_url}</li>                                                                    
+                        <li><a href="${i.website_url}" target="_blank">${i.website_url}</a></li>                                                                    
                     </ul>`
             })
             document.getElementById('brewery').style.display = "block";
@@ -35,30 +52,3 @@ function getClearData() {
     document.getElementById('City').value = "";
     document.getElementById('State').value = "";
 }
-
-function FormatPhoneNumber(num) {
-    var str = num.toString();
-
-    var matched = str.match(/\d+\.?\d*/g);
-
-    // 10 digit
-    if (matched.length === 3) {
-        return '(' + matched[0] + ') ' + matched[1] + '-' + matched[2];
-        // 7 digit
-    } else if (matched.length === 2) {
-        return matched[0] + '-' + matched[1];
-    }
-    // no formatting attempted only found integers (i.e. 1234567890)
-    else if (matched.length === 1) {
-        // 10 digit
-        if (matched[0].length === 10) {
-            return '(' + matched[0].substr(0, 3) + ') ' + matched[0].substr(3, 3) + '-' + matched[0].substr(6);
-        }
-        // 7 digit
-        if (matched[0].length === 7) {
-            return matched[0].substr(0, 3) + '-' + matched[0].substr(3);
-        }
-    }
-    // Format failed, return number back
-    return num;
-}    
